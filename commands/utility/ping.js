0b183bb8da2e859ec203edfb5a45f181269fda4a
mokotanin <titchou68@gmail.com>
2026-01-18 14:56:47 +0100

    fix: ping and ephemeral reload command response

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -3,8 +3,10 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong! and latency info'),
 	async execute(interaction) {
+		const sent = await interaction.reply({ content: 'Pinging...', withResponse: true });
 		const roundtrip = sent.resource.message.createdTimestamp - interaction.createdTimestamp;
 		const websocket = interaction.client.ws.ping;
-		const sent = await interaction.reply({ content:`Roundtrip: ${roundtrip}ms\nWebSocket: ${websocket}ms`, ephemeral:true});
+		await interaction.editReply(`Roundtrip: ${roundtrip}ms\nWebSocket: ${websocket}ms`);
+		setTimeout(() => interaction.deleteReply().catch(() => {}), 7000);
 	},
 };
--- commands/utility/reload.js
+++ commands/utility/reload.js
@@ -15,14 +15,14 @@ module.exports = {
 		const commandName = interaction.options.getString('command', true).toLowerCase();
 		const command = interaction.client.commands.get(commandName);
 		if (!command) {
-			return interaction.reply(`There is no command with name \`${commandName}\`!`);
+			return interaction.reply({ content:`There is no command with name \`${commandName}\`!`, ephemeral: true });
 		}
 
 		delete require.cache[require.resolve(`./${command.data.name}.js`)];
 		try {
 			const newCommand = require(`./${command.data.name}.js`);
 			interaction.client.commands.set(newCommand.data.name, newCommand);
-			await interaction.reply(`Command \`${newCommand.data.name}\` was reloaded!`);
+			await interaction.reply({ content:`Command \`${newCommand.data.name}\` was reloaded!`, ephemeral: true });
 		} catch (error) {
 			console.error(error);
 			await interaction.reply(
